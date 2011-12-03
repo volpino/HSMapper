a68abb427dc88b89f015db82230c34021ff9456f
volpino <fox91@anche.no>
2011-12-03 12:20:00 +0100

    gitignore and settings_local

--- findmyspot/settings.py
+++ findmyspot/settings.py
@@ -3,6 +3,8 @@
 import os
 import django
 
+from settings_local import *
+
 DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
