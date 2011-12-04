6ad3ffa6adfb7cd63840c7f1e27fe1d19c0386e3
volpino <fox91@anche.no>
2011-12-04 09:38:44 +0100

    more stuff angain!

--- findmyspot/core/views/api.py
+++ findmyspot/core/views/api.py
@@ -10,3 +10,52 @@ def get_hospitals(request):
     response = HttpResponse(mimetype="application/json")
     response.write(geojson_output)
     return response
+
+def edit_hospital(request):
+    result = "{success: false}"
+    if request.method == 'POST':
+        params = request.POST
+        try:
+            params_id = int(params["id"])
+        except ValueError:
+            params_id = -1
+        current_obj = HaitiHospitals.objects.get(id=params_id)
+        if current_obj:
+            for key in params:
+                if key != "id":
+                    setattr(current_obj, key, params[key])
+            current_obj.save()
+            result = "{success: true}"
+    response = HttpResponse(mimetype="application/json")
+    response.write(result)
+    return response
+
+def add_hospital(request):
+    result = "{success: false}"
+    if request.method == 'POST':
+        params = request.POST
+        current_obj = HaitiHospitals()
+        if current_obj:
+            for key in params:
+                if key != "id":
+                    setattr(current_obj, key, params[key])
+            current_obj.save()
+            result = "{success: true}"
+    response = HttpResponse(mimetype="application/json")
+    response.write(result)
+    return response
+
+def delete_hospital(request):
+    result = "{success: false}"
+    if request.method == 'POST':
+        params = request.POST
+        try:
+            params_id = int(params["id"])
+        except ValueError:
+            params_id = -1
+        current_obj = HaitiHospitals.objects.get(id=params_id)
+        current_obj.delete()
+        result = "{success: true}"
+    response = HttpResponse(mimetype="application/json")
+    response.write(result)
+    return response
