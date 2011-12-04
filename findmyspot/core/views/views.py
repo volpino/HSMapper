8b18f143837e058d262c770509f72b4f7570b61f
Davide Setti <davide.setti@gmail.com>
2011-12-04 15:44:39 +0100

    fix SERVER_IP in views

--- findmyspot/core/views/views.py
+++ findmyspot/core/views/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from django.conf import settings
 
 
 def home(request):
+    SERVER_IP = settings.SERVER_IP
     return render_to_response("home.html", locals(),
         context_instance=RequestContext(request)
     )
