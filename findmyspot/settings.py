5639e3831fca0cd305b8739bfa021feadceac46b
Davide Setti <davide.setti@gmail.com>
2011-12-04 09:53:14 +0100

    add gitignore, fix an importerror in the settings file, add a requirements file

--- findmyspot/settings.py
+++ findmyspot/settings.py
@@ -3,7 +3,10 @@
 import os
 import django
 
-from settings_local import *
+try:
+    from settings_local import *
+except ImportError:
+    pass
 
 DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
