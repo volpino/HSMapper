abb1a726dafc2e37040bcf2de7466b59c3f4ae7f
volpino <fox91@anche.no>
2012-02-26 13:33:01 +0100

    GeoDjango admin support and some translations

--- hsmapper/core/admin.py
+++ hsmapper/core/admin.py
@@ -2,20 +2,20 @@
 Admin module for hsmapper core app
 """
 
-from django.contrib import admin
+from django.contrib.gis import admin
 import reversion
 from core.models import Pathology, MedicalService, FacilityType, Facility, \
                     OpeningTime, SpecialDay
 
 
-class ReversionAdmin(reversion.VersionAdmin):
-    """ Wrapper class for django-reversion """
-    pass
+class FacilityAdmin(reversion.VersionAdmin, admin.GeoModelAdmin):
+    """ Wrapper class for django-reversion and geodjango """
+    list_display = ('name', 'address', 'facility_type')
 
 
 admin.site.register(Pathology)
 admin.site.register(MedicalService)
 admin.site.register(FacilityType)
-admin.site.register(Facility, ReversionAdmin)
+admin.site.register(Facility, FacilityAdmin)
 admin.site.register(OpeningTime)
 admin.site.register(SpecialDay)
