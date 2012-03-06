9dd7d655a61465a5b1a59552118dcca4b1565427
volpino <fox91@anche.no>
2012-03-06 15:47:54 +0100

    more tests on travis

--- hsmapper/settings.py
+++ hsmapper/settings.py
@@ -152,6 +152,17 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.csrf",
 )
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'hsmapper',                      # Or path to database file if using sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
 """
 LOGGING = {
     'version': 1,
